6fb59b7baf3a0eed8e04b35b49966600471add18
vividwhisp <korivinesh@gmail.com>
2026-05-03 07:32:21 +0530

    improve likes

--- app/like-button.js
+++ app/like-button.js
@@ -6,7 +6,7 @@ export default function LikeButton(){
     const [likes, setLikes] = useState(0);
 
     function handleClick() {
-    setLikes(likes + 1);
+    setLikes((currentlikes) => currentlikes + 1);
   }
  
   return <button onClick={handleClick}>Like ({likes})</button>
